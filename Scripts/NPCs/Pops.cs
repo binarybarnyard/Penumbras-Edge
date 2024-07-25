36477843cbd0442ebf57fb2b765dca9aadf4a30e
skathix <32309102+skathix@users.noreply.github.com>
2024-07-25 03:46:01 -0500

    UI: Updated the Dialog system, added a poorly drawn UI for dialog, but it works

--- Scripts/NPCs/Pops.cs
+++ Scripts/NPCs/Pops.cs
@@ -7,31 +7,23 @@ public partial class Pops : CharacterBody
     // R-G Tests:
     // [Passed] - NPC Appears on screen
     // [Passed] - NPC has an idle animation
-    // [Failed] - NPC Has proper collision
-    // [Failed] - NPC Responds to gravity
-    // [Failed] - NPC shifts idle animations randomly
-    // [Failed] - NPC changes to an idle animation when player is near
-    // [Failed] - NPC naturally returns to idle when the player leaves the area
+    // [Passed] - NPC Has proper collision
+    // [Passed] - NPC Responds to gravity
+    // [Passed] - NPC shifts idle animations randomly
+    // [Req Changed] - NPC changes to an idle animation when player is near
+    // [Req Changed] - NPC naturally returns to idle when the player leaves the area
     // [Failed] - NPC has a dialogue box that appears when the player is near
     // [Failed] - Player can interact with the NPC to trigger a console response
     // [Failed] - Player interaction causes a dialogue box to appear
     // [Failed] - Player interaction causes the NPC to change to a talking animation
     // [Failed] - NPC changes something about the player (Starting with adding a heart to the player)
 
-    // public nodes
+    // public variables
     private AnimatedSprite2D animatedSprite2D;
-    private Area2D triggerArea;
-    private CollisionShape2D collision;
     private Player player;
     private Timer tick;
     private string currentAnimation { get; set; }
     private bool animationPlayed { get; set; } = false;
-    private bool playerOverlapping { get; set; }
-    private DialogManager dialogManager { get; set; }
-
-    // public variables
-
-    // private variables
 
     // overrides
     public override void _Ready()
@@ -47,49 +39,19 @@ public partial class Pops : CharacterBody
         };
 
         animatedSprite2D.Play("idle0");
-        triggerArea = GetNode<Area2D>("TriggerArea");
-        collision = GetNode<CollisionShape2D>("TriggerArea/Collision");
         player = GetNode<Player>("/root/MainScene/Player");
         tick = GetNode<Timer>("Tick");
         tick.Autostart = true;
         currentAnimation = "idle1";
         GD.Print("Pops is ready!");
         tick.Connect("timeout", new Callable(this, nameof(AnimationCycle)));
-        triggerArea.Monitoring = true;
-
-        dialogManager = GetNode<DialogManager>("DialogManager");
-    }
-
-    public override void _Process(double delta)
-    {
-        if (triggerArea.OverlapsBody(player))
-        {
-            animatedSprite2D.Play("idle1");
-            playerOverlapping = true;
-
-            if (Input.IsActionJustPressed("attack"))
-            {
-                if (dialogManager.isDialogActive)
-                {
-                    dialogManager.OnNextButtonPressed();
-                }
-                else
-                {
-                    dialogManager.StartDialog(new string[] { "What're ya buyin'?", "Hehehe that's a good one, stranjuh.", "I'm just a humble merchant, but I'm sure we can make a deal." });
-                }
-            }
-        }
-        else
-        {
-            playerOverlapping = false;
-        }
     }
 
     private void AnimationCycle()
     {
         // Randomize the idle animation
         // let the last animation play out before changing
-        if (animationPlayed && !playerOverlapping)
+        if (animationPlayed)
         {
             var random = new RandomNumberGenerator();
             random.Randomize();
